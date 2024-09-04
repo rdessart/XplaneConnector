a49cfa54fec029b8bc68dd4ba0e3965ea9515e08
Romain DESSART <romaindessart@gmail.com>
2024-09-04 09:40:19 +0200

    Add project files.

---
+++ XplaneConnector/XplaneConnector/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace XplaneConnector.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
